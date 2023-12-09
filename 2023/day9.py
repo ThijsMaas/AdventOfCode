aecccaa3ea9da6aec93c1f1339e91d715882ad27
Thijs <tpwmaas@gmail.com>
2023-12-09 14:06:54 +0100

    day9

---
+++ 2023/day9.py
@@ -0,0 +1,50 @@
+from functools import reduce
+from io import TextIOWrapper
+
+
+def get_differences(numbers: list[int]):
+    differences = []
+    for i in range(1, len(numbers)):
+        differences.append(numbers[i] - numbers[i - 1])
+    return differences
+
+
+def extrapolate(numbers: list[int]):
+    difference_lists = [numbers]
+    while True:
+        differences = get_differences(numbers)
+        difference_lists.append(differences)
+        if all(d == 0 for d in differences):
+            break
+        numbers = differences
+    # Sum the last value in each list
+    return sum([l[-1] for l in difference_lists])
+
+
+def part_1(input: TextIOWrapper):
+    print("Part 1")
+    total = 0
+    for line in input.readlines():
+        numbers = [int(n) for n in line.split()]
+        next_number = extrapolate(numbers)
+        total += next_number
+    print(total)
+
+
+def part_2(input: TextIOWrapper):
+    print("Part 2")
+    total = 0
+    for line in input.readlines():
+        numbers = [int(n) for n in line.split()]
+        next_number = extrapolate(numbers[::-1])
+        total += next_number
+    print(total)
+
+
+def solution(input: TextIOWrapper, part_number: int):
+    if part_number == 1:
+        part_1(input)
+    elif part_number == 2:
+        part_2(input)
+    else:
+        raise ValueError(f"Invalid part number: {part_number}")
